e615958c59f2fd807fb9918061db8d1217743180
Allison Hu <pan.hu46@gmail.com>
2023-09-11 14:17:37 -0400

    Modify github workflows to build and upload react app

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-# Maven is used to build  and create a war file.
-mvn -Dmaven.test.skip=true clean install
-
-
+yarn install
+yarn build
+cd build
+zip -r ../hello-world.zip ./*
